fba3948987c7f4d9cb6061877ea8fb06d3c3d860
jhusting <joshhusting@gmail.com>
2019-04-27 23:23:37 -0700

    First Commit

---
+++ CMPM163_HW1/Assets/MovePoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePoint : MonoBehaviour
+{
+    private Vector3 StartPos;
+    private float ElapsedTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElapsedTime += Time.deltaTime;
+        //transform.position.x = StartPos.x + Mathf.Sin(Time.deltaTime * Mathf.PI);
+        Vector3 newPos = StartPos;
+        newPos.x += Mathf.Sin(ElapsedTime);
+
+        transform.position = newPos;
+    }
+}
---
+++ CMPM163_HW1B/Assets/ClickScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+    Renderer r;
+
+    private float val = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            val += 6f;
+
+            val %= 18f;
+
+            r.material.SetFloat("_Steps", val);
+        }
+    }
+}
